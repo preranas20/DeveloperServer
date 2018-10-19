c2f6ac0ac2700935d4ea4926a4849a5acd904972
preranas20 <Prerana.Singh@ramanalearningcenter.com>
2018-10-19 01:37:23 -0400

    add changes

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,23 @@ var app = express();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var postData = JSON.stringify({
-	apiKey:"VXLtyJ52I19G5v5M1p60V3cSRTHuHdqQC4fRPZpp",
-	phone:"+11",
-	messageText:" spent in coding hhhhhh",
+router.get('/', function(req1, res, next) {
+
+//copy from here
+
+	console.log("i am hitting my method");
+  const postData = JSON.stringify({
+	apiKey:"oiZTfmfBSIosY4aaJ6OeZA3xFek6LPbgfaUMtzqX",
+	phone:"+19804309833",
+	messageText:" developer is sending this text",
 	toPhoneNumber:"+17049066266"});
-});
+
+
 
 const options = {
-	host:'18.223.110.166',
+	hostname:'18.223.110.166',
 	port:5000,
-	path:"/sendToDevice",
+	path:"/user/sendToDevice",
 	method:'POST',
 	headers:{
 		'content-type': 'application/json'
@@ -27,25 +32,29 @@ const options = {
 };
 
 
-const req = http.request(options, function(response) {
+const postRequest = http.request(options, function(response) {
     response.setEncoding('utf8');
     response.on('data',function (chunk)  {
         console.log(`BODY: ${chunk}`);
     });
     response.on('end', function(){
         console.log('No more data in response.');
-        response.send('ok');
+        res.send('ok');
     });
-    // write data to request body
-    req.write(postData);
-    
-    req.end();
-});
-
-req.on('error', (e) => {
+    postRequest.on('error', (e) => {
     console.error(`problem with request: ${e.message}`);
 });
+   
+});
 
 
+ // write data to request body
+    postRequest.write(postData);
+    
+    postRequest.end();
+
+
+    //till here 
+});
 
 module.exports = router;
